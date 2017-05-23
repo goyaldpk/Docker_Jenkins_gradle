7eee619062ca78ec43e5f8dafa2dc4ced1e8d8eb
ADI TEJA <aditeja@triconinfotech.com>
2017-05-23 18:33:24 +0530

    Update

---
+++ src/test/java/com/andigital/andservice/read/api/services/ProjectsServiceImplTest.java
@@ -0,0 +1,15 @@
+package com.andigital.andservice.read.api.services;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by aditeja on 5/23/2017.
+ */
+public class ProjectsServiceImplTest {
+    @Test
+    public void testGetProjects() throws Exception {
+    }
+
+}
